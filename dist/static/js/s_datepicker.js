23cab1183a6ca68c29ef0b4184583312906bc2c5
shendan <664542051@qq.com>
2018-07-24 15:32:47 +0800

    修改日历

--- dist/static/js/s_datepicker.js
+++ dist/static/js/s_datepicker.js
@@ -9,31 +9,50 @@
     datepicker.buildUi = function(year, month) {
         monthData = datepicker.getMonthData(year, month);
 
+        var curmonth = $('input[name="selectedMonth"]').val();
+
         var html = '<div class="ui-datepicker-body">' +
             '<table>' +
             '<thead>' +
             '<tr>' +
-            '<th><div class="box"><div class="date">Mo</div></div></th>' +
-            '<th><div class="box"><div class="date">Tu</div></div></th>' +
-            '<th><div class="box"><div class="date">We</div></div></th>' +
-            '<th><div class="box"><div class="date">Th</div></div></th>' +
-            '<th><div class="box"><div class="date">Fr</div></div></th>' +
-            '<th><div class="box"><div class="date">Sa</div></div></th>' +
-            '<th><div class="box"><div class="date">Su</div></div></th>' +
+            '<th><div class="box">周一</div></th>' +
+            '<th><div class="box">周二</div></th>' +
+            '<th><div class="box">周三</div></th>' +
+            '<th><div class="box">周四</div></th>' +
+            '<th><div class="box">周五</div></th>' +
+            '<th><div class="box">周六</div></th>' +
+            '<th><div class="box">周日</div></th>' +
             '</tr>' +
             '</thead>' +
             '<tbody>';
 
         for (var i = 0; i < monthData.days.length; i++) {
             var date = monthData.days[i];
+
             if (i % 7 === 0) {
                 html += '<tr>';
             }
-            html += '<td data-date="' + date.date + '">' +
-                '<div class="box">' +
-                '<div class="date">' + date.showDate + '</div>' +
-                '</div>' +
-                '</td>';
+
+            if (date.month === 7 && date.showDate === 11) {
+                html += '<td data-date="' + date.date + '" style="background-color: #fef9ee">' +
+                    '<div class="box" style="' + (i > 34 ? 'border-bottom: none': '') + '">' +
+                    '<div class="date" style="color: #efae25">' + date.showDate + '</div>' +
+                    '<div class="event">' +
+                    '<p><i class="event-status event-status-green"></i><span class="event-text">已完成</span><label class="event-count">999</label></p>' +
+                    '<p><i class="event-status event-status-yellow"></i><span class="event-text">逾期</span><label class="event-count">8</label></p>' +
+                    '<p><i class="event-status event-status-red"></i><span class="event-text">审核不通过</span><label class="event-count">15</label></p>' +
+                    '</div>' +
+                    '</div>' +
+                    '</td>';
+            }
+            else {
+                html += '<td data-date="' + date.date + '">' +
+                    '<div class="box" style="' + (i > 34 ? 'border-bottom: none': '') + '">' +
+                    '<div class="date" style="color: ' + (date.month === parseInt(curmonth) ? '#333' : '#999') + '">' + date.showDate + '</div>' +
+                    '</div>' +
+                    '</td>';
+            }
+
             if (i % 7 === 6) {
                 html += '</tr>';
             }
